279692315a223ed7aa1c49bcb2d60fa68b06d210
George Politis <gp@jitsi.org>
2016-01-30 18:08:46 +0000

    Mavenizes the hammer.

--- jitsi-hammer.sh
+++ jitsi-hammer.sh
@@ -1,43 +1,25 @@
-#!/bin/bash
+#!/bin/sh -e
 
 if [ $# -lt 1 ] ; then #[ "$1" == "--help" ] || [ "$1" == "-help" ] || [ "$1" == "-h" ] || [ $# -lt 1 ] ; then
     set -- "-help"
 fi
 
 
-
-kernel="$(uname -s)"
-if [ $kernel == "Darwin" ] ; then
+KERN="$(uname -s)"
+if [ "$KERN" = "Darwin" ] ; then
 	SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-	
-	architecture="mac"
 else
 	SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
-	
-	if [ $kernel == "Linux" ] ; then
-		architecture="linux"
-	elif [ $kernel == "FreeBSD" ] ; then
-		architecture="freebsb"
-	else
-		echo "Unknown system : "$kernel
-		echo "Must be mac, linux, freebsb or windows"
-		exit 1
-	fi
-	
-	machine="$(uname -m)"
-	if [ "$machine" == "x86_64" ] || [ "$machine" == "amd64" ] ; then
-		architecture=$architecture"-64"
-	fi
 fi
 
-mainClass="org.jitsi.hammer.Main"
-classpath=$(JARS=($SCRIPT_DIR/jitsi-hammer.jar $SCRIPT_DIR/lib/*.jar); IFS=:; echo "${JARS[*]}")
-libs="$SCRIPT_DIR/lib/native/$architecture"
-logging_config="$SCRIPT_DIR/lib/logging.properties"
+# we can make this a script parameter at some point.
+REBUILD=true
+SC_HOME_DIR_NAME=.jitsi-hammer
+SC_HOME_DIR=~/$SC_HOME_DIR_NAME
+LOG_HOME=$SC_HOME_DIR/log
 
-LD_LIBRARY_PATH=$libs:$LD_LIBRARY_PATH java  \
-  -Djava.library.path=$libs \
-  -Djava.util.logging.config.file=$logging_config \
-  -Dnet.java.sip.communicator.SC_HOME_DIR_LOCATION=$SCRIPT_DIR \
-  -Dnet.java.sip.communicator.SC_HOME_DIR_NAME=.jitsi-hammer \
-  -cp $classpath $mainClass $@
+if $REBUILD ; then
+  mvn clean compile
+fi
+
+exec mvn exec:java -Dexec.args="$*" -Djava.util.logging.config.file=$SCRIPT_DIR/lib/logging.properties -Dnet.java.sip.communicator.SC_HOME_DIR_NAME="$SC_HOME_DIR_NAME" 2>&1 | tee $LOG_HOME/output.log
--- src/org/jitsi/hammer/Main.java
+++ src/org/jitsi/hammer/Main.java
@@ -22,7 +22,6 @@ import java.net.*;
 import java.util.List;
 
 import net.java.sip.communicator.impl.protocol.jabber.*;
-import net.java.sip.communicator.launcher.*;
 import net.java.sip.communicator.service.protocol.jabber.*;
 
 import org.jitsi.hammer.utils.*;
@@ -191,29 +190,6 @@ public class Main
             System.setProperty(
                 "sun.net.spi.nameservice.provider.1",
                 "dns,dnsjava");
-        }
-
-        if (version.startsWith("1.4") || vmVendor.startsWith("Gnu") ||
-            vmVendor.startsWith("Free"))
-        {
-            String os = "";
-
-            if (osName.startsWith("Mac"))
-                os = ChangeJVMFrame.MAC_OSX;
-            else if (osName.startsWith("Linux"))
-                os = ChangeJVMFrame.LINUX;
-            else if (osName.startsWith("Windows"))
-                os = ChangeJVMFrame.WINDOWS;
-
-            ChangeJVMFrame changeJVMFrame = new ChangeJVMFrame(os);
-            Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-
-            changeJVMFrame.setLocation(
-                screenSize.width/2 - changeJVMFrame.getWidth()/2,
-                screenSize.height/2 - changeJVMFrame.getHeight()/2);
-            changeJVMFrame.setVisible(true);
-
-            return;
         }
 
 
