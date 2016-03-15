2d308a36af35bd71d65eb84bd552f48ebf48bb8b
George Politis <gp@jitsi.org>
2016-03-15 22:37:43 +0000

    Makes it possible to connect to https BOSH.

--- jitsi-hammer.sh
+++ jitsi-hammer.sh
@@ -1,4 +1,29 @@
-#!/bin/sh -e
+#!/bin/bash -e
+
+# NOTE bash is required.
+
+KEYSTORE_FILE=keystore.ks
+KEYSTORE_PWD=123456
+
+if [ ! -f "$KEYSTORE_FILE" ]
+then
+    while getopts ":u:" o; do
+      case "${o}" in
+        u) BOSH_URI=${OPTARG}
+	  ;;
+      esac
+    done
+
+    OLDIFS="$IFS"
+    IFS='/' read -r -a array <<< "$BOSH_URI"
+    IFS="$OLDIFS"
+    if [ ${array[0]} = "https:" ]
+    then
+      CERT=$(mktemp /tmp/temporary-file.XXXXXXXX)
+      openssl s_client -showcerts -connect ${array[2]}:443 </dev/null 2>/dev/null|openssl x509 -outform PEM > $CERT
+      keytool -import -alias ${array[2]} -file $CERT -keystore "$KEYSTORE_FILE" -storepass "$KEYSTORE_PWD"
+    fi
+fi
 
 if [ $# -lt 1 ] ; then #[ "$1" == "--help" ] || [ "$1" == "-help" ] || [ "$1" == "-h" ] || [ $# -lt 1 ] ; then
     set -- "-help"
@@ -23,6 +48,8 @@ if $REBUILD ; then
 fi
 
 exec mvn exec:java -Dexec.args="$*" \
+  -Djavax.net.ssl.keyStore=$KEYSTORE_FILE -Djavax.net.ssl.keyStorePassword=$KEYSTORE_PWD \
+  -Djavax.net.ssl.trustStore=$KEYSTORE_FILE -Djavax.net.ssl.trustStorePassword=$KEYSTORE_PWD \
   -Djava.util.logging.config.file=$SCRIPT_DIR/lib/logging.properties \
   -Dnet.java.sip.communicator.SC_HOME_DIR_LOCATION=$SC_HOME_DIR_LOCATION \
   -Dnet.java.sip.communicator.SC_HOME_DIR_NAME=$SC_HOME_DIR_NAME 2>&1 | tee $LOG_HOME/output.log
